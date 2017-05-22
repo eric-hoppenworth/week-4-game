de37ba81fdc5d6dd45bc099b4935845fba07120d
eric hoppenworth <eric hoppenworth>
2017-05-22 09:48:34 -0400

    more work with buttons and moving

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -77,6 +77,9 @@ $(document).ready(function(){
 		levelUp(){
 
 		};
+		die(){
+
+		};
 
 	};
 
@@ -97,6 +100,7 @@ $(document).ready(function(){
 		//this.pnlEnemy.hide();
 		//fighting areas
 		this.arena = $("#arena");
+		this.attackButton = $('#attackButton');
 
 		//depricated
 		//create four object, one of each class.
@@ -138,7 +142,51 @@ $(document).ready(function(){
 			myGame.pnlEnemy.show();
 			//hide all thumbnails on player panel
 			myGame.pnlUser.find(".thumbnailHolder").hide();
+			//move character to arena
+			$("#arena0").attr("src",myGame.myFighter.imgSrc)
+			//hide corresponding thumbnail on enemy side
+			var holder = myGame.pnlEnemy.find(".thumbnailHolder");
+			holder.children().each(function(){
+					//double equals because one is expected to be a string
+				if($(this).attr("data-class") == myGame.myFighter.myClass ){
+					$(this).hide();
+				}
+			});
 
+		});
+
+		this.fightButton.on("click",function(){
+			//bring the enemy onto the board, and hide the button
+			myGame.myEnemy = new Fighter(myGame.unit,1);
+			$("#arena1").attr("src",myGame.myEnemy.imgSrc)
+			//hide corresponding thumbnail
+			var holder = myGame.pnlEnemy.find(".thumbnailHolder");
+			holder.children().each(function(){
+					//double equals because one is expected to be a string
+				if($(this).attr("data-class") == myGame.myEnemy.myClass ){
+					$(this).hide();
+				}
+			});
+			//hide fight button
+			myGame.fightButton.hide();
+			//show Health bars
+			//health bars are not yet created
+			
+			//show attackButton
+			myGame.attackButton.show();
+			//hide panels
+			myGame.pnlEnemy.hide();
+			myGame.pnlUser.hide();
+		});
+
+		this.attackButton.on("click",function(){
+			myGame.isDead = false;
+			//player attacks
+			myGame.myFighter.fight(myGame.myEnemy);
+			//enemy counters
+			if (myGame.isDead === false){
+				myGame.myEnemy.fight(myGame.myFighter);
+			}
 		});
 	};
 
