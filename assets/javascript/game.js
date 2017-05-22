6ca67b4dbce063d76a03b3cec9377bc2f4ed6d7b
eric hoppenworth <eric hoppenworth>
2017-05-22 11:26:37 -0400

    healthbars on screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ var healths = [100,120,130,80];
 var atts = [10,20,20,40];
 var growths = [15,5,10,5];
 var firstClick = false;
+var animationComplete = false;
 
 $(document).ready(function(){
 	//constructor for the fighter class.
@@ -26,31 +27,6 @@ $(document).ready(function(){
 			this.health = this.hitPoints;
 			this.attackPower = atts[this.myClass];
 			this.growth = growths[this.myClass];
-			if( myClass === 0 ){	
-				//sword
-				//mid HP
-				//low Att
-				//high Growth
-				this.className = "sword";
-			} else if( myClass === 1 ){
-				//axe
-				//mid HP
-				//mid Att
-				//mid Growth
-				this.className = "axe";
-			} else if( myClass === 2 ){
-				//lance
-				//high HP
-				//mid Att
-				//mid Growth
-				this.className = "lance";
-			} else if( myClass === 3 ){
-				//mage
-				//low HP
-				//high Att
-				//low Growth
-				this.className = "mage";
-			}
 
 			if (team === 1){
 				this.growth = 0;
@@ -60,19 +36,38 @@ $(document).ready(function(){
 				//grab HTML elements
 				//picture,HP,Att,Growth
 				this.myStatsHtml = [$("#leftPic"),$("#leftHP"),$("#leftAtt"),$("#leftGrowth")];
-				this.imgSrc = "assets/images/" + this.className + "L.png";
+				this.imgSrc = "assets/images/" + this.myClass + "L.png";
 			} else if(team === 1){
 				//grab HTML elements
 				//picture,HP,Att,Growth
 				this.myStatsHtml = [$("#rightPic"),$("#rightHP"),$("#rightAtt"),$("#rightGrowth")];
-				this.imgSrc = "assets/images/" + this.className + "R.png";
+				this.imgSrc = "assets/images/" + this.myClass + "R.png";
 			}
 			
 		};
 
 		//methods
-		fight(){
-
+		fight(enemy){
+			//because using 'this' inside the animate call back will grab the image element
+			var me = this
+			//animate the active unit
+			this.position.animate({'left' : '-=20px'},200);
+			this.position.animate({'left' : '+=40px'},200);
+			this.position.animate({'left' : '-=20px'},200).promise().done(function(){
+				//this will wait for the animation to finish
+				enemy.health -= me.attackPower + (me.growth*me.attackCount);
+				me.attackCount += 1;
+				me.showStats();
+				if (enemy.health <= 0 ){
+					enemy.health = 0;
+					enemy.showStats();
+					enemy.die();
+				} else {
+					enemy.showStats();
+					enemy.fight(me);
+				}
+			});
+			
 		};
 		levelUp(){
 
@@ -80,7 +75,16 @@ $(document).ready(function(){
 		die(){
 
 		};
+		showStats(){
+			//change HTML
+			var value = this.attackCount * this.growth
 
+			this.myStatsHtml[0].attr("src",this.imgSrc);
+			this.myStatsHtml[1].text(this.health + " / " + this.hitPoints);
+			this.myStatsHtml[2].text(this.attackPower + " + " + value);
+			this.myStatsHtml[3].text(this.growth);
+			//should also update health bars
+		};
 	};
 
 	function Game() {
@@ -102,14 +106,6 @@ $(document).ready(function(){
 		this.arena = $("#arena");
 		this.attackButton = $('#attackButton');
 
-		//depricated
-		//create four object, one of each class.
-		//they will all start as team = 1
-		// this.fighters = [];
-		// for (i =0;i < 4; i ++){
-		// 	this.fighters[i] = new Fighter(i,1);
-		// }
-
 		$(".thumbnail").on("click",function(event){
 			//this click function will allow the user to see the stats of each character.
 			//you will NOT actually pick a character here, that is located in the 'select' button
@@ -117,14 +113,11 @@ $(document).ready(function(){
 			//pull specific object
 			myGame.unit = parseInt($(this).attr("data-class"),10);
 
-			var myUnit = new Fighter(parseInt($(this).attr("data-class"),10),parseInt($(this).attr("data-team"),10));
-			//change HTML
-			var value = myUnit.attackCount * myUnit.growth
+			//print stats to HTML
 
-			myUnit.myStatsHtml[0].attr("src",myUnit.imgSrc);
-			myUnit.myStatsHtml[1].text(myUnit.health + " / " + myUnit.hitPoints);
-			myUnit.myStatsHtml[2].text(myUnit.attackPower + " + " + value);
-			myUnit.myStatsHtml[3].text(myUnit.growth);
+			var myUnit = new Fighter(parseInt($(this).attr("data-class"),10),parseInt($(this).attr("data-team"),10));
+			myUnit.showStats();
+			
 
 			//show select button, only if a unit is not already selected
 			if (firstClick === false ){
@@ -175,18 +168,18 @@ $(document).ready(function(){
 			//show attackButton
 			myGame.attackButton.show();
 			//hide panels
-			myGame.pnlEnemy.hide();
-			myGame.pnlUser.hide();
+			//myGame.pnlEnemy.hide();
+			//cmyGame.pnlUser.hide();
+			//enemy is not dead
+			myGame.isDead = false;
 		});
 
 		this.attackButton.on("click",function(){
-			myGame.isDead = false;
+			//hide button
+			myGame.attackButton.hide();
 			//player attacks
 			myGame.myFighter.fight(myGame.myEnemy);
-			//enemy counters
-			if (myGame.isDead === false){
-				myGame.myEnemy.fight(myGame.myFighter);
-			}
+
 		});
 	};
 
